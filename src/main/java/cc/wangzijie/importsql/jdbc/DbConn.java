dfa0e5d3c3f866ce5c43f359364144f4ed15def8
wangzijie <wzj_study@outlook.com>
2020-07-09 09:26:59 +0800

    配置正式库连接

--- src/main/java/cc/wangzijie/importsql/jdbc/DbConn.java
+++ src/main/java/cc/wangzijie/importsql/jdbc/DbConn.java
@@ -7,9 +7,9 @@ import java.sql.Statement;
 
 public class DbConn {
 
-    private static final String URL = "jdbc:oracle:thin:@10.72.12.197:1521:orcl";
+    private static final String URL = "jdbc:oracle:thin:@10.73.59.104:1521:ORCL_QMS";
     private static final String USERNAME = "DO_QMS";
-    private static final String PASSWORD = "doqms";
+    private static final String PASSWORD = "mwdoqms@18666@2019";
     private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
 
     private Connection conn = null;
